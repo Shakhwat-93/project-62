8236e27addff1ea89f9379dfe53e2f37eec5db89
shakhwat <raselbosss989@gmail.com>
2025-12-26 03:24:26 +0600

    upgraded design with Inter font and modern arrow icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['"Outfit"', 'sans-serif'],
+                sans: ['Inter', 'sans-serif'],
             },
             colors: {
                 brand: {
